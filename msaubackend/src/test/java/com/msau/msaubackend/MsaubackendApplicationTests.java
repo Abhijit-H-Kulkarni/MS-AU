1457d226bbe4d980e68fa651f42ebb8ea4005384
Abhijit H Kulkarni <abhijit.hkulkarni@accoliteindia.com>
2020-05-28 16:48:46 +0530

    initial Backend

---
+++ msaubackend/src/test/java/com/msau/msaubackend/MsaubackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.msau.msaubackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsaubackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
