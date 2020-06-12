cb61565e0cc2a1b5389ac5228f456d68311ee230
Abhijit H Kulkarni <abhijit.hkulkarni@accoliteindia.com>
2020-06-12 16:54:28 +0530

    Score Trend

--- src/app/components/view/view.component.ts
+++ src/app/components/view/view.component.ts
@@ -112,8 +112,8 @@ export class ViewComponent implements OnInit {
   }
 
   dataURItoBlob(dataURI) {
-    var byteString = atob(dataURI.split(',')[1]);
-    var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0];
+    var byteString = atob(dataURI);
+    var mimeString = "image/jpeg;base64";
     var ab = new ArrayBuffer(byteString.length);
     var ia = new Uint8Array(ab);
     for (var i = 0; i < byteString.length; i++) {
