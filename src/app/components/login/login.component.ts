e032ed5484e6a0b48013a08ffc0c65298639c473
Abhijit H Kulkarni <abhijit.hkulkarni@accoliteindia.com>
2020-06-23 13:36:29 +0530

    Final

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -49,13 +49,14 @@ export class LoginComponent implements OnInit {
           else
             localStorage.setItem("isadmin","false");
           this.loginService.getTrainer({tid:"",tname:"",designation:"",specialities:"",email:this.user_info.email}).subscribe(data=>{
+            console.log(data);
             if(data!=null)
               localStorage.setItem("isTrainer","true");
             else
               localStorage.setItem("isTrainer","false");
           });
           localStorage.setItem('welcome','true');
-          location.href="/assessment";
+          // location.href="/assessment";
           this.logger.info("Successful Login.");
         }
       }
@@ -80,7 +81,7 @@ export class LoginComponent implements OnInit {
       if(response.id!=null)
         this.loginService.findUser({email:response.email,uname:response.firstName,psw:""}).subscribe(data => {
           this.logger.info("Find User Event.");
-          this.loginService.getTrainer({tid:"",tname:"",designation:"",specialities:"",email:this.user.email}).subscribe(data=>{
+          this.loginService.getTrainer({tid:"",tname:"",designation:"",specialities:"",email:response.email}).subscribe(data=>{
             if(data!=null)
               localStorage.setItem("isTrainer","true");
             else
